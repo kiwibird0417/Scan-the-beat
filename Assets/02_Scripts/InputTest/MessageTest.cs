d5535da576c451d89db60620df5bafb805acf1ee
HyunSun Ahn <kiwibird0417@gmail.com>
2024-11-13 16:22:17 +0900

    Interactable 완성!

--- Assets/02_Scripts/InputTest/MessageTest.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class MessageTest : MonoBehaviour
-{
-    public void DisplaySelectMessage()
-    {
-        Debug.Log("선택되었다");
-    }
-}
---
+++ Assets/02_Scripts/InputTest/TriggerInteractable.cs
@@ -0,0 +1,73 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class TriggerInteractable : MonoBehaviour
+{
+    public InputActionReference triggerL;
+    public InputActionReference triggerR;
+    //public GameObject targetObject; // 삭제할 오브젝트를 인스펙터에서 설정
+
+    private bool isDestroyed = false; // 이미 삭제되었는지 확인하는 플래그
+
+    void Start()
+    {
+    }
+
+    public void OnDestroy()
+    {
+        // 이벤트 구독 해제
+        triggerL.action.performed -= OnTriggerL;
+        triggerR.action.performed -= OnTriggerR;
+    }
+
+    private void OnTriggerL(InputAction.CallbackContext context)
+    {
+        Event_Trigger_L();
+    }
+
+    private void OnTriggerR(InputAction.CallbackContext context)
+    {
+        Event_Trigger_R();
+    }
+
+    public void Display(){
+        triggerL.action.Enable();
+        triggerL.action.performed += OnTriggerL;
+
+        triggerR.action.Enable();
+        triggerR.action.performed += OnTriggerR;
+    }
+
+    public void Event_Trigger_L()
+    {
+        if (isDestroyed) return; // 이미 삭제되었다면 실행하지 않음
+
+        Debug.Log("왼쪽 트리거 버튼을 눌렀다");
+        DestroySelf();
+    }
+
+    public void Event_Trigger_R()
+    {
+        if (isDestroyed) return; // 이미 삭제되었다면 실행하지 않음
+
+        Debug.Log("오른쪽 트리거 버튼을 눌렀다");
+        DestroySelf();
+    }
+
+    private void DestroySelf()
+    {
+        isDestroyed = true; // 객체가 삭제됨을 표시
+        Destroy(this.gameObject); // targetObject 삭제
+            /*
+        if (targetObject != null)
+        {
+            isDestroyed = true; // 객체가 삭제됨을 표시
+            Destroy(GameObject); // targetObject 삭제
+        }
+        else
+        {
+            Debug.LogWarning("Target object is not assigned!");
+        }
+        */
+    }
+}
